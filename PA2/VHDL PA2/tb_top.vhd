64cbcd68f6eeb55cc9f5cfc2390fbd8a005d5fae
dmunizu <d.muniz@alumnos.upm.es>
2025-05-18 15:05:22 +0200

    PA2 top and testbench

--- PA2/VHDL PA2/tb_top.vhd
+++ PA2/VHDL PA2/tb_top.vhd
@@ -1,95 +1,105 @@
 library STD;
-use STD.textio.all;                     -- basic I/O
+use STD.textio.all; -- basic I/O
 library IEEE;
-use IEEE.std_logic_1164.all;            -- basic logic types
-use IEEE.std_logic_textio.all;          -- I/O for logic types
+use IEEE.std_logic_1164.all; -- basic logic types
+use IEEE.std_logic_textio.all; -- I/O for logic types
 
-LIBRARY IEEE;
-USE IEEE.std_logic_1164.all;
-USE IEEE.std_logic_arith.all;
-USE IEEE.std_logic_unsigned.all;
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.std_logic_arith.all;
+use IEEE.std_logic_unsigned.all;
 
-ENTITY tb_top IS 
-END tb_top;
+entity tb_top is
+end tb_top;
+architecture behavior of tb_top is
 
+  component top is
+    port (
+      reset, clk : in std_logic;
+      validacion : in std_logic;
+      data_in_0  : in std_logic_vector(23 downto 0);
+      data_in_1  : in std_logic_vector(23 downto 0);
+      data_in_2  : in std_logic_vector(23 downto 0);
+      data_out_0 : out std_logic_vector(23 downto 0);
+      data_out_1 : out std_logic_vector(23 downto 0);
+      data_out_2 : out std_logic_vector(23 downto 0);
+      valid_out  : out std_logic);
+  end component;
 
-ARCHITECTURE behavior OF tb_top IS
+  signal reset, clk                         : std_logic;
+  signal data_in_0, data_in_1, data_in_2    : std_logic_vector(23 downto 0);
+  signal data_out_0, data_out_1, data_out_2 : std_logic_vector(23 downto 0);
+  signal validacion                         : std_logic;
+  signal valid_out, ack_out                 : std_logic;
 
-COMPONENT top IS 
-  PORT (
-     reset, clk      : in std_logic;
-     validacion      : in std_logic;
-     data_in         : in std_logic_vector(23 downto 0); 
-     data_out        : out std_logic_vector(23 downto 0);  
-     valid_out       : out std_logic );
-END component;
+  constant period : time := 10 ns;
 
-  SIGNAL reset, clk : std_logic;
-  SIGNAL data_in, data_out : std_logic_vector(23 downto 0);
-  SIGNAL validacion : std_logic;
-  SIGNAL valid_out, ack_out : std_logic;
+  file f_out : TEXT is out "./f_out.txt"; -- ES PREFERIBLE PONER LA DIRECCCION COMPLETA!!
 
-  CONSTANT period : time := 10 ns;
+begin
 
-  FILE f_out : TEXT IS OUT "./f_out.txt";    -- ES PREFERIBLE PONER LA DIRECCCION COMPLETA!!
-  
-BEGIN  
+  -- UNIT UNDER TEST
+  UUT : top
+  port map
+  (
+    reset => reset, clk => clk,
+    validacion => validacion,
+    data_in_0 => data_in_0, data_in_1 => data_in_1, data_in_2 => data_in_2,
+    data_out_0 => data_out_0, data_out_1 => data_out_1, data_out_2 => data_out_2,
+    valid_out  => valid_out);
+  -- RESET
+  Proc_reset : process
+  begin
+    reset <= '0', '1' after 302 ns;
+    wait;
+  end process;
+  -- CLK
+  Proc_clk : process
+  begin
+    clk <= '0', '1' after period/2;
+    wait for period;
+  end process;
+  -- INPUT DATA
+  Proc_gen_data : process
+  begin
+    data_in_0  <= (others => '0');
+    data_in_1  <= (others => '0');
+    data_in_2  <= (others => '0');
+    validacion <= '0';
+    wait for 100 * period;
 
-      -- UNIT UNDER TEST
-      UUT : top 
-      PORT MAP (reset => reset, clk => clk, 
-      validacion => validacion, data_in => data_in, data_out => data_out, valid_out => valid_out );
+    data_in_0  <= "00000001" & "00000000" & "00000000";
+    validacion <= '1';
+    wait for period;
 
-	  
-      -- RESET
-      Proc_reset : PROCESS 
-	  BEGIN
-           reset <= '0', '1' after 302 ns; 
-		   wait;
-      END PROCESS;
-	 
-	 
-	  -- CLK
-      Proc_clk : PROCESS 
-	  BEGIN
-           clk <= '0', '1' after period/2; 
-		   wait for period;
-      END PROCESS;
-	  
-	  
-	  -- INPUT DATA
-      Proc_gen_data : PROCESS 
-	  BEGIN
-	       data_in <= (others => '0');   validacion <= '0';
-		   wait for 100*period;
-	 
-	       data_in <= "00000001" & "00000000" & "00000000";     validacion <= '1';
-		   wait for period;
+    data_in_0  <= (others => '0');
+    validacion <= '1';
+    wait for 50 * period;
 
-	       data_in <= (others => '0');   validacion <= '1';
-		   wait for 50*period;
-
-	       data_in <= (others => '0');   validacion <= '0';
-		   wait for 50*period;
-		   wait;
-      END PROCESS;
-	 
-
-	  -- OUTPUT DATA
-      Proc_save_data : PROCESS (clk)
-	    variable v_data_out : bit_vector(23 downto 0);
-	    variable v_linea : line;
-	  BEGIN
-   	       if (clk'event and clk='1') then
-               --ack_out <= '0' after 1 ns;
-               if (valid_out = '1') then
-                    --ack_out <= '1' after 2 ns;
-					v_data_out := To_BitVector(data_out);
-		            WRITE(v_linea, v_data_out);
-			        WRITELINE(f_out, v_linea);
-		       end if;
-		   end if;
-      END PROCESS;
-
-		 
-END behavior;
+    data_in_0  <= (others => '0');
+    validacion <= '0';
+    wait for 50 * period;
+    wait;
+  end process;
+  -- OUTPUT DATA
+  Proc_save_data : process (clk)
+    variable v_data_out : bit_vector(23 downto 0);
+    variable v_linea    : line;
+  begin
+    if (clk'event and clk = '1') then
+      --ack_out <= '0' after 1 ns;
+      if (valid_out = '1') then
+        --ack_out <= '1' after 2 ns;
+        v_data_out := To_BitVector(data_out_0);
+        WRITE(v_linea, v_data_out);
+        WRITELINE(f_out, v_linea);
+        v_data_out := To_BitVector(data_out_1);
+        WRITE(v_linea, v_data_out);
+        WRITELINE(f_out, v_linea);
+        v_data_out := To_BitVector(data_out_2);
+        WRITE(v_linea, v_data_out);
+        WRITELINE(f_out, v_linea);
+      end if;
+    end if;
+  end process;
+end behavior;
--- PA2/VHDL PA2/top.vhd
+++ PA2/VHDL PA2/top.vhd
@@ -1,79 +1,102 @@
-LIBRARY IEEE;
-USE IEEE.std_logic_1164.all;
-USE IEEE.std_logic_arith.all;
-USE IEEE.std_logic_unsigned.all;
+library IEEE;
+use IEEE.std_logic_1164.all;
+use IEEE.std_logic_arith.all;
+use IEEE.std_logic_unsigned.all;
 
-ENTITY top IS 
-  PORT (
-     reset, clk      : in std_logic;
-     validacion      : in std_logic;
-     data_in         : in std_logic_vector(23 downto 0); 
-     data_out        : out std_logic_vector(23 downto 0);  
-     valid_out       : out std_logic );
-END top;
+entity top is
+  port (
+    reset, clk : in std_logic;
+    validacion : in std_logic;
+    data_in_0  : in std_logic_vector(23 downto 0);
+    data_in_1  : in std_logic_vector(23 downto 0);
+    data_in_2  : in std_logic_vector(23 downto 0);
+    data_out_0 : out std_logic_vector(23 downto 0);
+    data_out_1 : out std_logic_vector(23 downto 0);
+    data_out_2 : out std_logic_vector(23 downto 0);
+    valid_out  : out std_logic);
+end top;
+architecture behavior of top is
 
+  component datapath is
+    port (
+      reset, clk : in std_logic;
+      estado     : in std_logic_vector(1 downto 0);
+      entradas_0 : in std_logic_vector(23 downto 0);
+      entradas_1 : in std_logic_vector(23 downto 0);
+      entradas_2 : in std_logic_vector(23 downto 0);
+      salidas_0  : out std_logic_vector(23 downto 0);
+      salidas_1  : out std_logic_vector(23 downto 0);
+      salidas_2  : out std_logic_vector(23 downto 0);
+      flags      : out std_logic_vector(7 downto 0));
+  end component;
 
-ARCHITECTURE behavior OF top IS
+  component control is
+    port (
+      reset, clk : in std_logic;
+      validacion : in std_logic;
+      flags      : in std_logic_vector(7 downto 0);
+      estado     : out std_logic_vector(1 downto 0);
+      fin        : out std_logic);
+  end component;
 
-COMPONENT datapath IS 
-  PORT (
-     reset, clk    : in std_logic;
-     comandos      : in std_logic_vector(7 downto 0);
-     entradas      : in std_logic_vector(23 downto 0);  
-     salidas       : out std_logic_vector(23 downto 0);  
-     flags         : out std_logic_vector(7 downto 0) );
-END component;
+  component interfaz_entrada is
+    port (
+      reset, clk : in std_logic;
+      validacion : in std_logic;
+      data_in_0  : in std_logic_vector(23 downto 0);
+      data_in_1  : in std_logic_vector(23 downto 0);
+      data_in_2  : in std_logic_vector(23 downto 0);
+      entradas_0 : out std_logic_vector(23 downto 0);
+      entradas_1 : out std_logic_vector(23 downto 0);
+      entradas_2 : out std_logic_vector(23 downto 0));
+  end component;
 
-COMPONENT control IS 
-  PORT (
-     reset, clk    : in std_logic;
-     validacion    : in std_logic;
-     flags         : in  std_logic_vector(7 downto 0);
-     comandos      : out std_logic_vector(7 downto 0);
-     fin           : out std_logic );
-END component;
+  component interfaz_salida is
+    port (
+      reset, clk : in std_logic;
+      fin        : in std_logic;
+      salidas_0  : in std_logic_vector(23 downto 0);
+      salidas_1  : in std_logic_vector(23 downto 0);
+      salidas_2  : in std_logic_vector(23 downto 0);
+      data_out_0 : out std_logic_vector(23 downto 0);
+      data_out_1 : out std_logic_vector(23 downto 0);
+      data_out_2 : out std_logic_vector(23 downto 0);
+      valid_out  : out std_logic);
+  end component;
+  signal entradas_0, entradas_1, entradas_2, salidas_0, salidas_1, salidas_2 : std_logic_vector(23 downto 0);
+  signal flags                                                               : std_logic_vector(7 downto 0);
+  signal estado                                                              : std_logic_vector(1 downto 0);
+  signal fin                                                                 : std_logic;
+begin
+  U1 : datapath
+  port map
+  (
+    reset => reset, clk => clk,
+    estado => estado,
+    entradas_0 => entradas_0, entradas_1 => entradas_1, entradas_2 => entradas_2,
+    salidas_0 => salidas_0, salidas_1 => salidas_1, salidas_2 => salidas_2,
+    flags  => flags);
 
-COMPONENT interfaz_entrada IS 
-  PORT (
-     reset, clk      : in std_logic;
-     validacion      : in std_logic;
-     data_in         : in std_logic_vector(23 downto 0); 
-     entradas        : out std_logic_vector(23 downto 0) ); 
-END component;
+  U2 : control
+  port map
+  (
+    reset => reset, clk => clk,
+    validacion => validacion, estado => estado, flags => flags, fin => fin);
 
-COMPONENT interfaz_salida IS 
-  PORT (
-     reset, clk      : in std_logic;
-     fin             : in std_logic;
-     salidas         : in std_logic_vector(23 downto 0);  
-     data_out        : out std_logic_vector(23 downto 0);  
-     valid_out       : out std_logic );
-END component;
+  U3 : interfaz_entrada
+  port map
+  (
+    reset => reset, clk => clk,
+    validacion => validacion,
+    data_in_0 => data_in_0, data_in_1 => data_in_1, data_in_2 => data_in_2,
+    entradas_0 => entradas_0, entradas_1 => entradas_1, entradas_2 => entradas_2);
 
-
-  SIGNAL entradas, salidas : std_logic_vector(23 downto 0);
-  SIGNAL comandos, flags : std_logic_vector(7 downto 0);
-  SIGNAL fin : std_logic;
-
-  
-BEGIN  
-
-
-U1 : datapath 
-     PORT MAP (reset => reset, clk => clk, 
-     comandos => comandos, entradas => entradas, salidas => salidas, flags => flags );
-
-U2 : control 
-     PORT MAP (reset => reset, clk => clk, 
-     validacion => validacion, comandos => comandos, flags => flags, fin => fin );
-
-U3 : interfaz_entrada  
-     PORT MAP (reset => reset, clk => clk, 
-     validacion => validacion, data_in => data_in, entradas => entradas );
-
-U4 : interfaz_salida  
-     PORT MAP (reset => reset, clk => clk, 
-     fin => fin, salidas => salidas, data_out => data_out, valid_out => valid_out );
-
-	 
-END behavior;
+  U4 : interfaz_salida
+  port map
+  (
+    reset => reset, clk => clk,
+    fin       => fin,
+    salidas_0 => salidas_0, salidas_1 => salidas_1, salidas_2 => salidas_2,
+    data_out_0 => data_out_0, data_out_1 => data_out_1, data_out_2 => data_out_2,
+    valid_out => valid_out);
+end behavior;
